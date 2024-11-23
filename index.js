de07899ef939a5acc17a2f5957891766e06752ed
SherzodToshpulatov <sherzodtoshpolatov5@gmail.com>
2024-11-23 11:08:24 +0500

    final

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 // import './scss/style.scss';
 
+const body = document.querySelector('body');
 const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
 const media = window.matchMedia('(width < 1110px)');
 navContent = document.querySelector('.nav__content');
+navOverlay = document.querySelector('.nav__overlay');
 
 function openMobileMenu(){
     btnOpen.setAttribute('aria-expanded', 'true');
+    body.classList.add('noscroll')
 }
+
+function closeMobileMenu(){
+    btnOpen.setAttribute('aria-expanded', 'false');
+    body.classList.remove('noscroll')
+}
+
+
 
 function setUpNav(e){
     if(e.matches){
-       navContent.style.display = 'block';
+       
+       
+       setTimeout(()=> {
+        navContent.style.display = 'block';
+        navOverlay.style.display = 'block';
+       }, 500);
     }
     else{
         navContent.style.display = '';
+        navOverlay.style.display = '';
     }
 }
 
 setUpNav(media);
 
 btnOpen.addEventListener('click', openMobileMenu);
+
+
+btnClose.addEventListener('click', closeMobileMenu);
+
 media.addEventListener('change', function(e){
 
     setUpNav(e)
